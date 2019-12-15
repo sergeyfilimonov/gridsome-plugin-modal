c31aee3c292e671c8b87a14188d482d5b301141e
Bruno Paz <brunopaz@sapo.pt>
2019-12-15 14:47:13 +0000

    Allow plugin to receive options

--- gridsome.client.js
+++ gridsome.client.js
@@ -3,6 +3,6 @@ import VModal from 'vue-js-modal/dist/ssr.index'
 export default function (Vue, options, { isServer: disabled, router }) {
   if(process.isClient) {
     const { default: VModal } = require('vue-js-modal')
-    Vue.use(VModal)
+    Vue.use(VModal, options)
   }
 }
